107445fd070b63d635e809c8fec7697ab7bd5af9
David Martin Saiz <davms81@gmail.com>
2020-08-07 15:40:27 +0200

    Kid name in label

--- KidsVaccineTracker/KidsVaccineTracker/Scenes/Initial/InitialViewController.swift
+++ KidsVaccineTracker/KidsVaccineTracker/Scenes/Initial/InitialViewController.swift
@@ -3,12 +3,14 @@ import UIKit
 import CoreData
 
 class InitialViewController: UIViewController {
+    @IBOutlet weak var kidLabel: UILabel!
     public var kids:[Kid]?
     @IBOutlet weak var kid_photo: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         kid_photo.image = kids?.first?.photo
+        kidLabel.text = kids?.first?.name
     }
 }
 
